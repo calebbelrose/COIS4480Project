f6a1c090df7e8f3ca5c38cdf5a89205c4fcb03db
calebbelrose <calebbelrose@gmail.com>
2016-04-03 17:52:53 -0400

    timer
    timer

--- COIS 4480H Project/Assets/Scripts/Timer.cs
+++ COIS 4480H Project/Assets/Scripts/Timer.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static float timer;
+    public static bool timeStarted = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 120;
+        timeStarted = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeStarted == true)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void OnGUI()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        GUIStyle myStyle = new GUIStyle();
+        myStyle.fontSize = 50;
+
+        if (timer > 11)
+            myStyle.normal.textColor = Color.yellow;
+        else if (timer < 1)
+            myStyle.normal.textColor = Color.blue;
+        else
+            myStyle.normal.textColor = Color.red;
+
+        GUI.Label(new Rect(Screen.width / 2 - 50, 10, 100, 100), niceTime, myStyle);
+    }
 }
