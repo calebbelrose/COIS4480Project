12d19c689c4b41fda2cfc1e8961950371c3de9b5
calebbelrose <calebbelrose@gmail.com>
2016-04-03 16:11:37 -0400

    testttt

--- COIS 4480H Project/Assets/Scripts/Timer.cs
+++ COIS 4480H Project/Assets/Scripts/Timer.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class Timer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public static float timer;
+    public static bool timeStarted = false;
+
+    // Use this for initialization
+    void Start () {
+        timer = 120;
+        timeStarted = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (timeStarted == true)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    void OnGUI()
+    {
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        GUIStyle myStyle = new GUIStyle();
+        myStyle.fontSize = 50;
+        if(seconds > 10)
+            myStyle.normal.textColor = Color.yellow;
+        else
+            myStyle.normal.textColor = Color.red;
+        GUI.Label(new Rect(Screen.width/2 - 50, 10, 100, 100), niceTime, myStyle);
+    }
 }
