439e3bb63197f9f262efdf301addd4f386cfef6c
JoeTelford <Joe.T.Telford@gmail.com>
2016-04-03 17:47:44 -0400

    AI script?

---
+++ COIS 4480H Project/Assets/Scripts/AIController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+    Vector3 startPosition;
+    int roamRadius = 1;
+	// Use this for initialization
+	void Start ()
+    {
+        startPosition = Random.insideUnitSphere;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * roamRadius;
+        randomDirection += startPosition;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
+        Vector3 finalPosition = hit.position;
+        //_nav.destination = finalPosition;
+    }
+}
