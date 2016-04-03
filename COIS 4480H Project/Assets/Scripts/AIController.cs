618164982c152ee727b9a201baa94c9ce95f69fa
calebbelrose <calebbelrose@gmail.com>
2016-04-03 18:54:50 -0400

    ai
    ai

--- COIS 4480H Project/Assets/Scripts/AIController.cs
+++ COIS 4480H Project/Assets/Scripts/AIController.cs
@@ -3,24 +3,40 @@ using System.Collections;
 
 public class AIController : MonoBehaviour {
 
-    public Vector3 startPosition;
-    public int roamRadius = 1;
+    public Transform[] points;
     public float moveSpeed;
+    public Transform goal;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
     // Use this for initialization
     void Start ()
     {
-        moveSpeed = 1f;
-        startPosition = Random.insideUnitSphere;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        transform.Translate(Random.value * 8f - 4f, 0.29f, Random.value * 8f - 4f);
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
     {
-        Vector3 randomDirection = Random.insideUnitSphere * roamRadius;
-        randomDirection += startPosition;
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, roamRadius, 1);
-        Vector3 finalPosition = hit.position;
-        //_nav.destination = finalPosition;
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (agent.remainingDistance < 0.5f)
+            GotoNextPoint();
     }
 }
