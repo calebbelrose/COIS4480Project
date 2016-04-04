bfc828a6abc7011dacf2eb809716cb9e5340538c
calebbelrose <calebbelrose@gmail.com>
2016-04-04 11:00:51 -0400

    MORE STUFF
    MORE STUFF

--- COIS 4480H Project/Assets/Scripts/PlayerController.cs
+++ COIS 4480H Project/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,6 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour {
 
-    void Start()
-    {
-        Color[] colors = { Color.blue, Color.cyan, Color.gray, Color.green, Color.magenta, Color.red, Color.white, Color.yellow };
-        Color currColor;
-        Renderer[] renderers;
-
-        transform.Translate(Random.value * 8f - 4f, 0.3047705f, Random.value * 8f - 4f);
-        renderers = gameObject.GetComponentsInChildren<Renderer>();
-
-        for (int i = 0; i < renderers.Length; i++)
-        {
-            currColor = colors[(int)(Random.value * colors.Length)];
-            renderers[i].material.color = currColor;
-        }
-    }
-
     void Update()
     {
         float translation = Input.GetAxis("Vertical");
