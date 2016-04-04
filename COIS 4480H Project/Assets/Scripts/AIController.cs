4e57d4858869fe0039b8718ca7f7e4e32f5b950a
calebbelrose <calebbelrose@gmail.com>
2016-04-03 22:38:29 -0400

    changed movement
    changed movement

--- COIS 4480H Project/Assets/Scripts/AIController.cs
+++ COIS 4480H Project/Assets/Scripts/AIController.cs
@@ -1,24 +1,26 @@
-﻿using UnityEngine;
+﻿
+
+
+using UnityEngine;
 using System.Collections;
 
-public class AIController : MonoBehaviour {
+public class AIController : MonoBehaviour
+{
 
     private GameObject[] targets;
     public Transform goal;
-    private int destPoint = 0;
+    private int destPoint;
     private NavMeshAgent agent;
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         transform.Translate(Random.value * 8f - 4f, 0.33f, Random.value * 8f - 4f);
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = false;
         targets = GameObject.FindGameObjectsWithTag("NavMeshTarget");
-        GUIStyle myStyle = new GUIStyle();
-        myStyle.fontSize = 50;
+        destPoint = (int)(Random.value * targets.Length);
         gameObject.GetComponent<Renderer>().material.color = Color.green;
-        GUI.Label(new Rect(Screen.width / 2 - 50, 300, 100, 100), gameObject.GetComponent<Renderer>().material.color.ToString(), myStyle);
         GotoNextPoint();
     }
 
@@ -33,13 +35,21 @@ public class AIController : MonoBehaviour {
 
         // Choose the next point in the array as the destination,
         // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % targets.Length;
+        destPoint = (int)(Random.value * targets.Length);
     }
 
     // Update is called once per frame
-    void Update ()
+    void Update()
     {
         if (agent.remainingDistance < 0.5f)
+        {
+            StartCoroutine(Wait());
             GotoNextPoint();
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(Random.value + 2.5f);
     }
 }
--- COIS 4480H Project/Assets/Scripts/PlayerController.cs
+++ COIS 4480H Project/Assets/Scripts/PlayerController.cs
@@ -4,45 +4,19 @@ using System.Collections;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour {
 
-    // Update is called once per frame
-    public float moveSpeed;
-
     void Start()
     {
-        moveSpeed = 1f;
+        transform.Translate(Random.value * 8f - 4f, 0.33f, Random.value * 8f - 4f);
     }
 
     void Update()
     {
-        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
-        /* CharacterController controller = GetComponent<CharacterController>();
-        if (Input.GetButtonDown("W"))
-        {
-            Vector3 forward = transform.TransformDirection(Vector3.forward);
-            float curSpeed = speed * Input.GetAxis("Vertical");
-            controller.SimpleMove(forward * curSpeed);
-        }
-
-        if (Input.GetButtonDown("S"))
-        {
-            Vector3 backward = transform.TransformDirection(-Vector3.forward);
-            float curSpeed = speed * Input.GetAxis("Vertical");
-            controller.SimpleMove(backward * curSpeed);
-        }
-
-        if (Input.GetButtonDown("A"))
-        {
-            Vector3 left = transform.TransformDirection(Vector3.left);
-            float curSpeed = speed * Input.GetAxis("Vertical");
-            controller.SimpleMove(left * curSpeed);
-        }
-
-        if (Input.GetButtonDown("D"))
-        {
-            Vector3 right = transform.TransformDirection(-Vector3.left);
-            float curSpeed = speed * Input.GetAxis("Vertical");
-            controller.SimpleMove(right * curSpeed);
-        }*/
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime * 100;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
     }
 
     void FixedUpdate ()
