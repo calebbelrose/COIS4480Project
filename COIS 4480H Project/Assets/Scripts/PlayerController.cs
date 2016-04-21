6f02d2d0f4fda6ec480a091f34e0337049c37296
calebbelrose <calebbelrose@gmail.com>
2016-04-21 19:42:03 -0400

    fixed ai

--- COIS 4480H Project/Assets/Scripts/PlayerController.cs
+++ COIS 4480H Project/Assets/Scripts/PlayerController.cs
@@ -19,9 +19,4 @@ public class PlayerController : MonoBehaviour
         rotation *= Time.deltaTime * 100;
         transform.Rotate(0, rotation, 0);
     }
-
-    void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        Debug.Log(hit.collider.gameObject.ToString());
-    }
 }
--- COIS 4480H Project/Assets/Scripts/Timer.cs
+++ COIS 4480H Project/Assets/Scripts/Timer.cs
@@ -151,7 +151,7 @@ public class Timer : MonoBehaviour
         for (int i = 0; i < aiList.Length; i++)
         {
             NavMeshHit hit;
-            NavMesh.SamplePosition(new Vector3(Random.value * 8f - 4f, 0f, Random.value * 8f - 4f), out hit, 1.0f, NavMesh.AllAreas);
+            NavMesh.SamplePosition(new Vector3(Random.value * 8f - 4f, 0.190027f, Random.value * 8f - 4f), out hit, 1f, NavMesh.AllAreas);
             aiList[i].gameObject.transform.position = hit.position;
             renderers = aiList[i].gameObject.GetComponentsInChildren<Renderer>();
             aiList[i].gameObject.GetComponent<NavMeshAgent>().enabled = true;
